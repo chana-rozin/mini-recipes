d51bd29cefb154a8e4ec399711254f224d3d3fc4
GlickYael <glick.yael3@gmail.com>
2024-11-10 12:18:48 +0200

    get and post catagories

---
+++ src/app/api/categories/route.ts
@@ -0,0 +1,17 @@
+import { NextResponse } from "next/server";
+import { getAllDocuments , insertDocument } from '@/services/mongo'
+
+export async function GET(request: Request) {
+    const data = await getAllDocuments('catagories');
+    return NextResponse.json({
+        data
+    })
+}
+
+export async function POST(request: Request){
+    const body = await request.json();
+    const res = await insertDocument('users', body)
+    return NextResponse.json({
+        res
+    })
+}
