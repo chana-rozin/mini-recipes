26721800f0b786f7fc5c255ea64b9e5d6ccb638a
GlickYael <glick.yael3@gmail.com>
2024-11-10 13:09:31 +0200

    post catagory

--- src/app/api/categories/route.ts
+++ src/app/api/categories/route.ts
@@ -10,7 +10,7 @@ export async function GET(request: Request) {
 
 export async function POST(request: Request){
     const body = await request.json();
-    const res = await insertDocument('users', body)
+    const res = await insertDocument('categories', body)
     return NextResponse.json({
         res
     })
