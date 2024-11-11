f27b3a53e504ca566e66cf0df9a511a75eea8a72
GlickYael <glick.yael3@gmail.com>
2024-11-11 10:23:40 +0200

    fix

--- src/components/PopUpCard/PopUpCard.tsx
+++ src/components/PopUpCard/PopUpCard.tsx
@@ -2,49 +2,49 @@ import { useState } from 'react';
 import styles from './PopUpCard.module.css';
 
 interface PopUpCardProps {
-  mealName: string;
-  category: string[];
-  ingredients: string[];
-  instructions: string;
-  isFavorite: boolean;
-  onClose: () => void;
+    mealName: string;
+    category: string[];
+    ingredients: string[];
+    instructions: string;
+    isFavorite: boolean;
+    onClose: () => void;
 }
 
 const PopUpCard: React.FC<PopUpCardProps> = ({ mealName, category, ingredients, instructions, isFavorite, onClose }) => {
-  const [favorite, setFavorite] = useState(isFavorite);
+    const [favorite, setFavorite] = useState(isFavorite);
 
-  const toggleFavorite = () => {
-    setFavorite(!favorite);
-  };
+    const toggleFavorite = () => {
+        setFavorite(!favorite);
+    };
 
-  return (
-    <div className={styles.popupContainer}>
-      <div className={styles.closeButton} onClick={onClose}>
-        ×
-      </div>
-      <div className={styles.card}>
-        <h2 className={styles.mealName}>{mealName}</h2>
-        <div className={styles.category}>
-          <span>Categories: </span>{category}
+    return (
+        <div className={styles.popupContainer}>
+            <div className={styles.closeButton} onClick={onClose}>
+                ×
+            </div>
+            <div className={styles.card}>
+                <h2 className={styles.mealName}>{mealName}</h2>
+                <div className={styles.category}>
+                    <span>Categories: </span>{category}
+                </div>
+                <div className={styles.favorite} onClick={toggleFavorite}>
+                    {favorite ? '★' : '☆'} {/* Full or hollow star */}
+                </div>
+                <div className={styles.ingredients}>
+                    <h3 className={styles.h3}>Ingredients</h3>
+                    <ul>
+                        {ingredients.map((ingredient, index) => (
+                            <li key={index}>{ingredient}</li>
+                        ))}
+                    </ul>
+                </div>
+                <div className={styles.instructions}>
+                    <h3 className={styles.h3}>Instructions</h3>
+                    <p>{instructions}</p>
+                </div>
+            </div>
         </div>
-        <div className={styles.favorite} onClick={toggleFavorite}>
-          {favorite ? '★' : '☆'} {/* Full or hollow star */}
-        </div>
-        <div className={styles.ingredients}>
-          <h3 className={styles.h3}>Ingredients</h3>
-          <ul>
-            {ingredients.map((ingredient, index) => (
-              <li key={index}>{ingredient}</li>
-            ))}
-          </ul>
-        </div>
-        <div className={styles.instructions}>
-          <h3 className={styles.h3}>Instructions</h3>
-          <p>{instructions}</p>
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default PopUpCard;
